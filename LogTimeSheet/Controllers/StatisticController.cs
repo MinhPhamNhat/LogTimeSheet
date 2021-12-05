d17d96ebf76f3332b8a5c7894722f00cc319996e
MinhPhamNhat <73160254+MinhPhamNhat@users.noreply.github.com>
2021-12-05 11:41:59 +0700

    Add statistic

---
+++ LogTimeSheet/Controllers/StatisticController.cs
@@ -0,0 +1,42 @@
+﻿using LogTimeSheet.Config;
+using LogTimeSheet.Models;
+using LogTimeSheet.Repo;
+using LogTimeSheet.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace LogTimeSheet.Controllers
+{
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+    public class StatisticController : ApiController
+    {
+        private SystemContext db = new SystemContext();
+        private StatisticDAO statisticDAO;
+        private Jwt jwtValidator = new Jwt();
+
+        // GET: api/Statistic
+        /// <summary>
+        /// - Role: Admin
+        /// - Desc: Lấy total projects, total PM, total employee, total logs, total users
+        /// </summary>
+        [HttpGet]
+        [Route("api/Statistic")]
+        [AuthorizeRequest("All")]
+        public IHttpActionResult GetAll()
+        {
+            statisticDAO = new StatisticDAO(db);
+            return Ok(new { 
+                countUser = statisticDAO.numberOfUser(),
+                countStaff = statisticDAO.numberOfStaffs(),
+                countPM = statisticDAO.numberOfPMs(),
+                countProject = statisticDAO.numberOfProjects(),
+                countLog = statisticDAO.numberOfLogs(),
+            });
+        }
+    }
+}
---
+++ LogTimeSheet/Repo/StatisticDAO.cs
@@ -0,0 +1,54 @@
+﻿using LogTimeSheet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogTimeSheet.Repo
+{
+    public class StatisticDAO
+    {
+        SystemContext db;
+        public StatisticDAO(SystemContext db)
+        {
+            this.db = db;
+        }
+
+        public int numberOfUser()
+        {
+            var count = db.Users
+            .Count();
+            return count;
+        }
+
+        public int numberOfStaffs()
+        {
+            var count = db.Users
+            .Where(u => u.Role == 2)
+            .Count();
+            return count;
+        }
+
+        public int numberOfPMs()
+        {
+            var count = db.Users
+            .Where(u => u.Role == 1)
+            .Count();
+            return count;
+        }
+
+        public int numberOfProjects()
+        {
+            var count = db.Projects
+            .Count();
+            return count;
+        }
+
+        public int numberOfLogs()
+        {
+            var count = db.Logs
+            .Count();
+            return count;
+        }
+    }
+}
