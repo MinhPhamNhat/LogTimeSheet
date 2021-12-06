a48e4e6a22f3a8666602812a3111ff5190934e83
MinhPhamNhat <73160254+MinhPhamNhat@users.noreply.github.com>
2021-12-06 17:39:13 +0700

    Context

--- LogTimeSheet/Models/SystemContext.cs
+++ LogTimeSheet/Models/SystemContext.cs
@@ -8,6 +8,8 @@ namespace LogTimeSheet.Models
 {
     public class SystemContext : DbContext
     {
+        //"Data Source=103.151.241.83;Initial Catalog=LogTimeSheet;User ID=sa;Password=Sa@123456;"
+        //"Data Source=MinhPham;Initial Catalog=LogTimeSheet;Integrated Security=true;"
         public SystemContext() : base("Data Source=103.151.241.83;Initial Catalog=LogTimeSheet;User ID=sa;Password=Sa@123456;") { }
         public DbSet<User> Users { get; set; }
         public DbSet<Log> Logs { get; set; }
